2e3fc66b30687925964336d762daa1e08f0368e4
dramsammy <dramsammy@stuy.edu>
2018-10-23 11:50:34 -0400

    Began to work on SuperArray ------ confused on appending arrays?

---
+++ SuperArray.java
@@ -0,0 +1,31 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+  public SuperArray(String[] a,int b){
+    data = a;
+    size = b;
+  }
+  public void clear(){
+    String[] data = new String[size];
+  }
+  public int size(){
+    return size;
+  }
+  public boolean isEmpty(){
+    return size == 0;
+  }
+  public booleanAdd(String element){
+    if (data[size-1] != null){
+      for(int i = 0; i < data.length; i++){
+
+
+      }
+    }
+  })
+
+
+
+
+
+
+}
